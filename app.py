780d941519bf840e66e90f93ca6d31d4f64e93c5
aaron-clarusway <67314728+aaron-clarusway@users.noreply.github.com>
2020-08-23 12:46:44 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import streamlit as st
+df = pd.read_csv("https://docs.google.com/spreadsheets/d/1kqgVJzMFxsI2NYXinxpjOdcVn9RBVDwoD_Phckhqp5I/export?format=csv&gid=0",
+                 index_col="Student Number")
+st.title('Your Rank')
+df[df.index == st.text_input("Enter your student number: ")].T
