dc9c3f9e7562e5777086885d93e232920fb56cce
aaron-clarusway <67314728+aaron-clarusway@users.noreply.github.com>
2020-09-14 00:39:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import streamlit as st
-df = pd.read_csv("https://docs.google.com/spreadsheets/d/1kqgVJzMFxsI2NYXinxpjOdcVn9RBVDwoD_Phckhqp5I/export?format=csv&gid=0",
+df = pd.read_csv("https://docs.google.com/spreadsheets/d/12-hdqmjPtVBrzYfiD-W3WivGLLQnnBwqpyh0Z9uMOhs/export?format=csv&gid=0",
                  index_col="Student Number")
 st.title('Student Report')
 df[df.index == st.text_input("Enter your student number: ")].T
+
